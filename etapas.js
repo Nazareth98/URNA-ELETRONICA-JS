d015e4ce0f4b8f0910949fe83488a6767aa3261c
Nazareth98 <patrick98live@gmail.com>
2022-01-04 10:59:51 -0300

    Att

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12300,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '12300.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 50111,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'50111.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 10,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'10.png', legenda: 'Prefeito'},
+                    {url:'10-2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 500,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url:'50.png', legenda: 'Prefeito'},
+                    {url:'50-2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
